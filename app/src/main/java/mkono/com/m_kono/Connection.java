1fb857fea425998f5aa8415afcc24e97b45708bb
FelixWaweru <wawerufelix60@gmail.com>
2017-07-06 10:26:55 +0300

    Initial Upload

---
+++ app/src/main/java/mkono/com/m_kono/Connection.java
@@ -0,0 +1,15 @@
+package mkono.com.m_kono;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by USER PC on 06-Jul-17.
+ */
+public class Connection extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connection);
+    }
+}
---
+++ app/src/main/java/mkono/com/m_kono/splash.java
@@ -0,0 +1,40 @@
+package mkono.com.m_kono;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+/**
+ * Created by USER PC on 05-Jul-17.
+ */
+public class splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        final TextView iv = (TextView) findViewById(R.id.text);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.fadein);
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                Intent i = new Intent(splash.this,MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
